402b72702ea9604de752790456231aa6b115d692
hackerboy09 <isc1930319@gmail.com>
2023-03-01 09:28:31 -0600

    Lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  const nombre = "Jose Antonio Rendon Cruz"; "nombre";
+  return ( 
+  <div> 
+  <h1>Lista de hobbies de {nombre}</h1>
+  <ol>
+  <li>Jugar Xbox</li>
+  <li>Practicar Ajedrez</li>
+  <li>Salir a correr</li>
+  <li>Pasear a mi perro</li>
+  <li>Aprender otro idioma</li>
+  </ol>
+  </div>
+  )
+}
+
+export default App
